7de1cac3e43f335d9e6d106a1052c6a1e7dc8e58
Justin Rodrigues <dvanisound@Justins-MacBook-Pro.local>
2023-03-13 19:08:22 -0500

    testing

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,21 +1,21 @@
 
-import { open, username, password, submit, getErrorMessage } from '../pageobjects/login.page.js';
+const LoginPage = require('../pageobjects/login.page.js');
 
 describe('Login functionality', () => {
     it('should allow user to login with valid credentials', () => {
-        open();
-        username.setValue('standard_user');
-        password.setValue('secret_sauce');
-        submit();
+        LoginPage.open();
+        LoginPage.username.setValue('standard_user');
+        LoginPage.password.setValue('secret_sauce');
+        LoginPage.submit();
         expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
     });
     browser.pause(5000);
     it('should display error message with invalid credentials', () => {
-        open();
-        username.setValue('invalidusername');
-        password.setValue('invalidpassword');
-        submit();
-        const errorMsg = getErrorMessage();
+        LoginPage.open();
+        LoginPage.username.setValue('invalidusername');
+        LoginPage.password.setValue('invalidpassword');
+        LoginPage.submit();
+        const errorMsg = LoginPage.getErrorMessage();
         expect(errorMsg).toEqual('Epic sadface: Username is required');
     });
 });
